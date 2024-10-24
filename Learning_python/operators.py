5a7072bce930334aa27ec93b3b0f84a01a94a615
Annsrutto <annsrutto567@gmail.com>
2024-10-24 14:02:07 +0300

    operators

---
+++ Learning_python/operators.py
@@ -0,0 +1,26 @@
+# 1. Arithmetic operators
+# x + y - c = z
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+sum = num1 + num2
+print("The sum of {} and {} is {}".format(num1, num2, sum))
+
+# # 2. Assignment operator =
+# name = Anns
+
+# Comparison operators
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+if num1 == num2:
+    print("The numbers are equal")
+else:
+    print("The numbers are not equal")
+
+
+if num1 != num2:
+    print("The numbers are not equal")
+else:
+    print("the numbers are equal")
