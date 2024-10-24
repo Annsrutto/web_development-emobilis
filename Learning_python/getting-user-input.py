412a13c3a6160de64d43a6b1d7aa8810c93092e2
Annsrutto <annsrutto567@gmail.com>
2024-10-24 12:37:30 +0300

    how to get user input

---
+++ Learning_python/getting-user-input.py
@@ -0,0 +1,6 @@
+# How to get user input
+
+first_name = input("Enter your first name: ")
+email = input("Enter your email address: ")
+
+print("Your first name is {}, and your email address is {}".format(first_name, email))
