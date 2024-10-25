c135de17ed9cd891993841de7634cf4a0a38e4f7
Annsrutto <annsrutto567@gmail.com>
2024-10-25 11:12:50 +0300

    Assignment

---
+++ Learning_python/Assignment1.py
@@ -0,0 +1,8 @@
+saved_passwd = "Anns567#"
+
+user_passwd = input("Enter your password: ")
+
+if user_passwd == saved_passwd:
+    print("Permission granted🙂")
+else:
+    print("Permission denied🥲")
